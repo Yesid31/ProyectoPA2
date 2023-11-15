68d311121884c9fba2a8af64b1531e40c7d532d9
EstebanHernandezA16 <EstebanHernandez23201@hotmail.com>
2023-11-15 07:02:34 -0500

    creando Menu

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,12 +6,58 @@ export function Home (){
     return (
 
         <>
-        <div className="banner"></div>
+        <div className="banner">
         <div className="row">
-        <div className="col-12 col-md"></div>
+    <div className="col-12 col-md-6">
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+            <i className="bi bi-person-badge"></i>
+            </span>
+            <input type="text" className="form-control" placeholder="Nombre Cliente" />
+        </div>
+    </div>
+    <div className="col-12 col-md-6">
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+            <i className="bi bi-envelope-fill"></i>
+            </span>
+            <input type="text" className="form-control" placeholder="Correo cliente" />
+        </div>
+    </div>
+</div>
+
+<div className="row">
+    <div className="col-12">
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">@</span>
+            <input type="text" className="form-control" placeholder="Telefono Cliente" />
+        </div>
+    </div>
+</div>
+
+<div className="row">
+    <div className="col-12 col-md-6">
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">@</span>
+            <input type="date" className="form-control" placeholder="Telefono Cliente" />
+        </div>
+    </div>
+    
+</div>
+        </div>
+        <div className="row">
+        <div className="col-12 col-md">
+        <form>
+
+
+
+
+
+</form>
+        </div>
 
         </div>
-        <Formulario/>
+        
         </>
     )
 
